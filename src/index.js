23297756f045ab27deb5f96c11b962affe5fbff7
Aslanov Rustam <aslanovrg@gmail.com>
2020-11-05 23:55:23 +0200

    добавляет разметку HTМL и CSS, шаблон .hbs и функцию прорисовки шаблона

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './styles.css';
+import countryCard from './country-template.hbs';
+
+// console.log(countryCard())
+fetch('https://restcountries.eu/rest/v2/name/ukraine').then(response => {
+    return response.json();
+}).then(country => {console.log(country); const markup = countryCard(country[0]); console.log(markup);} ).catch(error => console.log(error));
