5da6ea392f124738cf3d5c5055f4a948eea75ac5
Aslanov Rustam <aslanovrg@gmail.com>
2020-11-07 00:24:58 +0200

    фиксит код для рендера корректной разметки по событию input

--- src/index.js
+++ src/index.js
@@ -1,7 +1,36 @@
 import './styles.css';
 import countryCard from './country-template.hbs';
+import debounce from 'lodash.debounce';
 
 // console.log(countryCard())
-fetch('https://restcountries.eu/rest/v2/name/ukraine').then(response => {
+const cardContainer = document.querySelector('.js-card-container');
+const inputValue = document.querySelector('.input-search');
+
+// inputValue.addEventListener('input', onSearch)
+// inputValue.addEventListener('input', debounce(() => { onSearch(); }, 500));
+inputValue.addEventListener('input', debounce(onSearch, 500));
+
+function onSearch(e) {
+    e.preventDefault();  
+    // const form = e.currentTarget;
+    const form = inputValue;
+    // const form = e.currentTarget.elements.query.value;
+    const searchCountry = form.value;
+    //  const searchCountry = form.elements.query.value;
+    fetchCountryByName(searchCountry)
+    .then(renderCountryCard)
+    .catch(error => console.log(error));
+}
+
+function renderCountryCard(country) {
+    const markup = countryCard(country[0]);
+    cardContainer.innerHTML = markup;
+};
+
+function fetchCountryByName(countryName) {
+   return fetch(`https://restcountries.eu/rest/v2/name/${countryName}`).then(response => {
     return response.json();
-}).then(country => {console.log(country); const markup = countryCard(country[0]); console.log(markup);} ).catch(error => console.log(error));
+    })
+    // .then(renderCountryCard)
+    // .catch (error => console.log(error)); 
+};
